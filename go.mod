f71f94101060c321c5c74a54aaeef484bf7e64ae
jesus m. rodriguez <jesusr@redhat.com>
2021-04-14 10:28:04 -0400

    Initial image extractor

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/jmrodri/image-extractor
+
+go 1.15
+
+require (
+	github.com/Shopify/logrus-bugsnag v0.0.0-20171204204709-577dee27f20d // indirect
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/bshuster-repo/logrus-logstash-hook v1.0.0 // indirect
+	github.com/operator-framework/operator-registry v1.17.0
+	github.com/sirupsen/logrus v1.8.1
+	rsc.io/letsencrypt v0.0.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	registryimage "github.com/operator-framework/operator-registry/pkg/image"
+	"github.com/operator-framework/operator-registry/pkg/image/containerdregistry"
+	log "github.com/sirupsen/logrus"
+)
+
+func DiscardLogger() *log.Entry {
+	logger := log.New()
+	logger.SetOutput(ioutil.Discard)
+	return log.NewEntry(logger)
+}
+
+func ExtractImage(ctx context.Context, logger *log.Entry, image string) (string, error) {
+	if logger == nil {
+		logger = DiscardLogger()
+	}
+
+	// Use a temp directory for image files.
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	imageDir, err := ioutil.TempDir(wd, "image-")
+	if err != nil {
+		return "", err
+	}
+	// This should always work, but if it doesn't imageDir is still valid.
+	if dir, err := filepath.Rel(wd, imageDir); err == nil {
+		imageDir = dir
+	}
+	// Export the image into imageDir
+	logger = logger.WithFields(log.Fields{"dir": imageDir})
+
+	// Use a containerd registry instead of shelling out to a container tooll.
+	reg, err := containerdregistry.NewRegistry(containerdregistry.WithLog(logger))
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if err := reg.Destroy(); err != nil {
+			logger.WithError(err).Warn("Error destroying local cache")
+		}
+	}()
+
+	// Pull the image if it isn't present locally
+	// if !local {
+	if err := reg.Pull(ctx, registryimage.SimpleReference(image)); err != nil {
+		return "", fmt.Errorf("error pulling image %s: %v", image, err)
+	}
+	// }
+
+	// Unpack the image's contents.
+	if err := reg.Unpack(ctx, registryimage.SimpleReference(image), imageDir); err != nil {
+		return "", fmt.Errorf("error unpacking image %s: %v", image, err)
+	}
+
+	return imageDir, nil
+}
+
+func main() {
+	logger := log.New()
+	logger.SetOutput(os.Stdout)
+	log.NewEntry(logger)
+
+	image := "gcr.io/kubebuilder/kube-rbac-proxy:v0.8.0"
+	outputDir, err := ExtractImage(context.Background(), nil, image)
+	if err != nil {
+		fmt.Printf("Error loading image: %v\n", err)
+		os.Exit(-1)
+	}
+	fmt.Printf("Image extracted to %v\n", outputDir)
+}
